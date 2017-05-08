b67987af407c11ee74854199aab54f329be1dc17
Laobe <laobility@gmail.com>
2017-05-08 13:37:25 +0800

    update

--- python/algorithm/LCM.py
+++ python/algorithm/LCM.py
@@ -2,3 +2,45 @@
 
 """ 最大公共子矩阵
 """
+
+A = [[0, -2, -7, 0],
+     [9, 2, -6, 2],
+     [-4, 1, -4, 1],
+     [-1, 8, 0, -2],]
+N = 4
+F = []
+Q = []
+P = []
+
+
+def LCS(lists=None):
+    if lists is None:
+        return 0
+
+    max_sum = 0
+    all_sum = 0
+    for i in lists:
+        all_sum += i
+        if all_sum < 0:
+            all_sum = 0
+        else:
+            max_sum = max(max_sum, all_sum)
+    return max_sum
+
+
+def LCM():
+    max_sum = 0
+    for i in range(len(A)):
+        for j in range(i+1, len(A)):
+            F = []
+            for k in range(len(A[j])):
+                F.append(sum(A[k][i:j]))
+            if max_sum <= LCS(F):
+                max_sum = LCS(F)
+                right = i
+                left = j
+    return max_sum, right, left
+
+
+if __name__ == '__main__':
+    print(LCM())
--- python/algorithm/LCS.py
+++ python/algorithm/LCS.py
@@ -4,4 +4,54 @@
 """
 
 
+def MaxSubSequence1(lists=None):
+    """ 枚举法 O(N^3)
+    """
+    if lists == None:
+        lists = []
+    max_sum = 0
+    for i in range(len(lists)):
+        this_sum = 0
+        for j in range(i, len(lists)):
+            this_sum = sum(lists[i:j+1])
+            max_sum = max(max_sum, this_sum)
+    return max_sum
 
+
+def MaxSubSequence2(lists=None):
+    """ 枚举法 O(N^2)
+    """
+    if lists == None:
+        lists = []
+    max_sum = 0
+    for i in range(len(lists)):
+        this_sum = 0
+        for j in range(i, len(lists)):
+            this_sum += lists[j]
+            max_sum = max(max_sum, this_sum)
+    return max_sum
+
+
+def MaxSubSequence4(lists=None):
+    """ 动态规划法 O(N)
+    """
+    if lists == None:
+        lists = []
+    max_sum = 0
+    this_sum = 0
+    for j in range(len(lists)):
+        this_sum += lists[j]
+        if this_sum < 0:
+            this_sum = 0
+        elif this_sum > max_sum:
+            max_sum = this_sum
+        print(this_sum)
+    return max_sum
+
+
+
+if __name__ == '__main__':
+    lists = [11, -32, 14]
+    print(MaxSubSequence1(lists))
+    print(MaxSubSequence2(lists))
+    print(MaxSubSequence4(lists))
